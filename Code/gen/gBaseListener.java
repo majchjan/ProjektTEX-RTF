7f507375f771c066f8546c4559d69155b3530afb
majchjan <157830574+majchjan@users.noreply.github.com>
2024-05-09 16:38:06 +0200

    plik .g4

---
+++ Code/gen/gBaseListener.java
@@ -0,0 +1,135 @@
+// Generated from D:/Programs/IntelliJ/saves/testANTLR/g.g4 by ANTLR 4.13.1
+
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+/**
+ * This class provides an empty implementation of {@link gListener},
+ * which can be extended to create a listener which only needs to handle a subset
+ * of the available methods.
+ */
+@SuppressWarnings("CheckReturnValue")
+public class gBaseListener implements gListener {
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterStart(gParser.StartContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitStart(gParser.StartContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterHdr(gParser.HdrContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitHdr(gParser.HdrContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterCont(gParser.ContContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitCont(gParser.ContContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterSect(gParser.SectContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitSect(gParser.SectContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterTkt(gParser.TktContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitTkt(gParser.TktContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterLst(gParser.LstContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitLst(gParser.LstContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterBrc(gParser.BrcContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitBrc(gParser.BrcContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterTm(gParser.TmContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitTm(gParser.TmContext ctx) { }
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterEveryRule(ParserRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitEveryRule(ParserRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void visitTerminal(TerminalNode node) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void visitErrorNode(ErrorNode node) { }
+}
---
+++ Code/gen/gListener.java
@@ -0,0 +1,89 @@
+// Generated from D:/Programs/IntelliJ/saves/testANTLR/g.g4 by ANTLR 4.13.1
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+
+/**
+ * This interface defines a complete listener for a parse tree produced by
+ * {@link gParser}.
+ */
+public interface gListener extends ParseTreeListener {
+	/**
+	 * Enter a parse tree produced by {@link gParser#start}.
+	 * @param ctx the parse tree
+	 */
+	void enterStart(gParser.StartContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link gParser#start}.
+	 * @param ctx the parse tree
+	 */
+	void exitStart(gParser.StartContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link gParser#hdr}.
+	 * @param ctx the parse tree
+	 */
+	void enterHdr(gParser.HdrContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link gParser#hdr}.
+	 * @param ctx the parse tree
+	 */
+	void exitHdr(gParser.HdrContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link gParser#cont}.
+	 * @param ctx the parse tree
+	 */
+	void enterCont(gParser.ContContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link gParser#cont}.
+	 * @param ctx the parse tree
+	 */
+	void exitCont(gParser.ContContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link gParser#sect}.
+	 * @param ctx the parse tree
+	 */
+	void enterSect(gParser.SectContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link gParser#sect}.
+	 * @param ctx the parse tree
+	 */
+	void exitSect(gParser.SectContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link gParser#tkt}.
+	 * @param ctx the parse tree
+	 */
+	void enterTkt(gParser.TktContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link gParser#tkt}.
+	 * @param ctx the parse tree
+	 */
+	void exitTkt(gParser.TktContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link gParser#lst}.
+	 * @param ctx the parse tree
+	 */
+	void enterLst(gParser.LstContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link gParser#lst}.
+	 * @param ctx the parse tree
+	 */
+	void exitLst(gParser.LstContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link gParser#brc}.
+	 * @param ctx the parse tree
+	 */
+	void enterBrc(gParser.BrcContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link gParser#brc}.
+	 * @param ctx the parse tree
+	 */
+	void exitBrc(gParser.BrcContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link gParser#tm}.
+	 * @param ctx the parse tree
+	 */
+	void enterTm(gParser.TmContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link gParser#tm}.
+	 * @param ctx the parse tree
+	 */
+	void exitTm(gParser.TmContext ctx);
+}
